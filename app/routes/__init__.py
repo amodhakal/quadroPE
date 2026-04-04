764bc0616ba54d4dfc8b2f36fb5c823654ae8fbe
MavScript.blu <darienmaverick@outlook.com>
2026-04-04 12:58:04 -0500

    (#70)feat: add /logs endpoing for viewing logs without SSH

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,8 +3,10 @@ def register_routes(app):
     from app.routes.urls import urls_bp
     from app.routes.events import events_bp
     from app.routes.metrics import metrics_bp
+    from app.routes.logs import logs_bp
 
     app.register_blueprint(users_bp)
     app.register_blueprint(urls_bp)
     app.register_blueprint(events_bp)
     app.register_blueprint(metrics_bp)
+    app.register_blueprint(logs_bp)
